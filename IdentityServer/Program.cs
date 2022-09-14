331e41d1054d94684fe5d52661ec7471b37958bc
Anton <gold-library@yandex.ru>
2022-09-14 18:23:54 +0300

    Initial. Add migrations for postgres

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,55 @@
+using System.Reflection;
+using IdentityServer.Data;
+using IdentityServer.Factories;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, dbContextOptionsBuilder) =>
+{
+    dbContextOptionsBuilder.UseNpgsql(
+        serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString("Identity"), NpgsqlOptionsAction);
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipalFactory>()
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddConfigurationStore(configurationStoreOptions =>
+    {
+        configurationStoreOptions.ResolveDbContextOptions = ResolveDbContextOptions;
+    })
+    .AddOperationalStore(operationalStoreOptions =>
+    {
+        operationalStoreOptions.ResolveDbContextOptions = ResolveDbContextOptions;
+    });
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+
+    await scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.MigrateAsync();
+}
+
+app.Run();
+
+void NpgsqlOptionsAction(NpgsqlDbContextOptionsBuilder npgsqlDbContextOptionsBuilder)
+{
+    npgsqlDbContextOptionsBuilder.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+}
+
+void ResolveDbContextOptions(IServiceProvider serviceProvider, DbContextOptionsBuilder dbContextOptionsBuilder)
+{
+    dbContextOptionsBuilder.UseNpgsql(
+        serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString("Identity"),
+        NpgsqlOptionsAction);
+}
